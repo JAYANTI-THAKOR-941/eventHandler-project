7b507050a6666f00309332aba91c6cab3356247b
JAYANTI THAKOR <jyantithakor941@gmail.com>
2026-02-23 11:15:41 +0530

    Edit and Delete todo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,62 +1,82 @@
 import { useState } from "react";
 import './todo.css';
 
-const Todo = ()=>{
+const Todo = () => {
 
-    const [form,setForm] = useState({text:"",status:""});
+    const [form, setForm] = useState({ text: "", status: "" });
 
-    const [tasks,setTasks] = useState([]);
+    const [tasks, setTasks] = useState([]);
 
-    const [error,setError] = useState("");
+    const [editIndex, setEditIndex] = useState(null);
 
-    const handleChange = (e)=>{
-        setForm({...form,[e.target.name]:e.target.value});
+    const [error, setError] = useState("");
+
+    const handleChange = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value });
     }
 
-    const handleSubmit = (e)=>{
+    const handleSubmit = (e) => {
         e.preventDefault();
 
-        if(form.text === ""){
+        if (form.text === "") {
             setError("Todo name is required.!!");
+            return;
         }
-        else{
-            setTasks([...tasks,form]);
+
+        if (editIndex !== null) {
+            const updatedTask = tasks.map((t, i) => (i === editIndex ? form : t))
+
+            setTasks(updatedTask);
+            setEditIndex(null);
+
+        } else {
+            setTasks([...tasks, form]);
             setError("");
-            setForm({text:"",status:""});
+            setForm({ text: "", status: "" });
         }
+
     }
 
+    const handleDelete = (index) => {
+        setTasks(tasks.filter((t, i) => i !== index));
+    }
 
-    if(error) return <h2>{error}</h2>;
+    const handleEdit = (index) => {
+        setForm(tasks[index]);
+        setEditIndex(index)
+    }
 
-    return(
+    if (error) return <h2>{error}</h2>;
+
+    return (
         <div className="todo-container">
             <form onSubmit={handleSubmit} className="todo-form">
                 <h2>Task Management</h2>
-                <input type="text" name="text" value={form.text} onChange={handleChange} placeholder="Enter todo name"  />
-                <input type="text" name="status" value={form.status} onChange={handleChange} placeholder="Enter todo status"  />
+                <input type="text" name="text"  value={form.text} onChange={handleChange} placeholder="Enter todo name" />
+                <input type="text" name="status" value={form.status} onChange={handleChange} placeholder="Enter todo status" />
                 <button>Add task</button>
-            </form>  
+            </form>
 
             <table>
-                    <tr>
-                        <th>No.</th>
-                        <th>Task Name</th>
-                        <th>Task Status</th>
-                        <th>Action</th>
-                    </tr>
-                    {
-                        tasks.map((t,i)=>(
-                            <tr key={i}>
-                                <td>{i+1}</td>
-                                <td>{t.text}</td>
-                                <td>{t.status}</td>
-                                <td><button>Delete</button></td>
-                            </tr>
-                        ))
-                    }
+                <tr>
+                    <th>No.</th>
+                    <th>Task Name</th>
+                    <th>Task Status</th>
+                    <th colSpan={2}>Action</th>
+                </tr>
+                {
+                    tasks.map((t, i) => (
+                        <tr key={i}>
+                            <td>{i + 1}</td>
+                            <td>{t.text}</td>
+                            <td>{t.status}</td>
+                            <td><button onClick={()=>handleEdit(i)}>Edit</button></td>
+                            <td><button onClick={() => handleDelete(i)}>Delete</button></td>
+                        </tr>
+                    ))
+                }
             </table>
-            
+
         </div>
     )
 }
